74b8c6e0f8254af6a5e4dc7278a851c053506dbd
Jack Bush <bush.jack@gmail.com>
2015-07-02 19:11:14 +0100

    pseudocode

--- js/script.js
+++ js/script.js
@@ -2,20 +2,31 @@ var document, window, THREE;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var light = new THREE.AmbientLight( 0x404040 );
+var light = new THREE.AmbientLight( 0xbadce6 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshBasicMaterial( { color: 0xe63f52 } );
 var cube = new THREE.Mesh( geometry, material );
 
 scene.add( cube );
 scene.add( light );
 
-camera.position.z = 3;
+camera.position.z = 5;
+
+// macro for cubes
+
+// y - to top screen then subsequently += random number
+// x - set as 2 * width * random-0.5
+// z - amplitude * rand-.5
+
+// color assignment
+
+// hex codes from array at random
+
 
 var zoomOut = function () {
   camera.position.z += 0.001;
@@ -28,7 +39,7 @@ var render = function () {
   renderer.render( scene, camera );
   cube.rotation.y += 0.01;
   cube.rotation.z += 0.01;
-  zoomOut();
+  // zoomOut();
 };
 
 render();
